a6efca7b3f10df7460d682f363afe053f0b36cf0
Vegardxoxo <vnhenrik@stud.ntnu.no>
2023-10-03 20:58:15 +0200

    Worked on queries

--- queries_vegard.py
+++ queries_vegard.py
@@ -1,4 +1,8 @@
+from multiprocessing import Pool, cpu_count
+
+import numpy as np
 import pandas as pd
+from haversine import haversine
 from tabulate import tabulate
 
 from DbConnector import DbConnector
@@ -35,120 +39,140 @@ class task2:
         return rows
 
 
-def main():
-    program = None
-    try:
-        program = task2()
-        program.show_tables()
-        query2 = """
-         SELECT user_id,
-            AVG(trackpoint_count) AS avg_trackpoints_per_activity,
-             MIN(trackpoint_count) AS min_trackpoints,
-            MAX(trackpoint_count) AS max_trackpoints
-
-            FROM
-                (
-                SELECT user_id, COUNT(activity_id) AS trackpoint_count
-                FROM Activity
-                INNER JOIN TrackPoint ON Activity.id = TrackPoint.activity_id
-                GROUP BY user_id, activity_id
-            ) AS ActivityData
-            GROUP BY user_id
-        """
-        # program.execute_sql_query(query2)
-        query5 = """
-            SELECT user_id, COUNT(DISTINCT (transportation_mode)) AS unique_modes
+def query2(program):
+    query = """
+        WITH ActivityData AS (
+            SELECT user_id, COUNT(activity_id) AS trackpoint_count
             FROM Activity
-            WHERE Transportation_mode IS NOT NULL
+            INNER JOIN TrackPoint ON Activity.id = TrackPoint.activity_id
+            GROUP BY user_id, activity_id
+            )
+            SELECT user_id,
+            AVG(trackpoint_count) AS avg_trackpoints_per_activity,
+            MIN(trackpoint_count) AS min_trackpoints,
+            MAX(trackpoint_count) AS max_trackpoints
+            FROM ActivityData
             GROUP BY user_id
-            ORDER BY unique_modes DESC
-            LIMIT 10
-        """
-        # program.execute_sql_query(query5)
-        create_temp_table = """
-        CREATE TEMPORARY TABLE TempOverlappingActivities AS
-        SELECT 
-            a1.id AS activity_id1,
-            a2.id AS activity_id2
-        FROM 
-            Activity AS a1
-        JOIN 
-            Activity AS a2 ON a1.user_id != a2.user_id
-        WHERE 
-            a2.start_date_time <= a1.end_date_time
-            AND a2.end_date_time >= a1.start_date_time;
-                """
-        query8 = """
-                SELECT 
-            a1.user_id AS user1,
-            a2.user_id AS user2,
-            tp1.lat AS lat1,
-            tp1.lon AS lon1,
-            tp1.date_time AS date_time1,
-            tp2.lat AS lat2,
-            tp2.lon AS lon2,
-            tp2.date_time AS date_time2
-        FROM 
-            TempOverlappingActivities oa
-        JOIN 
-            Activity a1 ON oa.activity_id1 = a1.id
-        JOIN 
-            Activity a2 ON oa.activity_id2 = a2.id
-        JOIN 
-            TrackPoint tp1 ON oa.activity_id1 = tp1.activity_id
-        JOIN 
-            TrackPoint tp2 ON oa.activity_id2 = tp2.activity_id;
-        """
-        results = program.execute_sql_query(create_temp_table)
-        print(results)
-        program.execute_sql_query(query8)
+            
+    """
+    program.execute_sql_query(query)
 
 
+def query5(program):
+    query5 = """
+        SELECT user_id, COUNT(DISTINCT (transportation_mode)) AS unique_modes
+        FROM Activity
+        WHERE Transportation_mode IS NOT NULL
+        GROUP BY user_id
+        ORDER BY unique_modes DESC
+        LIMIT 10
+    """
+    program.execute_sql_query(query5)
 
 
-
-
-
-
-
-
-
-        df = pd.read_sql_query(query8, program.db_connection)
-        df.to_csv('data2.csv', index=False)
-        print("Saved to csv.")
-        df = pd.read_csv('data.csv')
-        df_reduced = df[0:100000]
-        df_reduced.to_csv('data_reduced.csv', index=False)
-        print("Read csv into memory")
-        df['date_time'] = pd.to_datetime(df['date_time'])
-        df = df.sort_values(by=['activity_id'])
-
-        close_users = set()
-        for _, row1 in df.iterrows():
-            time_delta = pd.Timedelta(seconds=30)
-        # Find users who have invalid activies (trackpoints deviate with 5 minutes) and count them
-        query11 = """
-          WITH UserTrackPoints AS (
+def query11(program):
+    query11 = """
+        WITH UserTrackPoints AS (
             SELECT user_id, TrackPoint.id as track_point_id, date_time
             FROM Activity
             INNER JOIN TrackPoint ON Activity.id = TrackPoint.activity_id
             )
             SELECT a.user_id,  COUNT(DISTINCT (a.track_point_id)) AS invalid_trackpoints
             FROM UserTrackPoints a
-            JOIN UserTrackPoints b ON a.track_point_id = b.track_point_id - 1 AND a.user_id = b.user_id
+            JOIN UserTrackPoints b ON a.track_point_id = b.track_point_id + 1 AND a.user_id = b.user_id
             WHERE ABS(TIMESTAMPDIFF(MINUTE , a.date_time, b.date_time)) >= 5  
             GROUP BY a.user_id
-        """
-    # program.execute_sql_query(query11)
+            ORDER BY invalid_trackpoints DESC
+    """
+    program.execute_sql_query(query11)
+
+
+def query8(program):
+    query_entire = ("""
+    WITH OverlappingActivities AS (
+        SELECT  
+            a.user_id as user1_id,
+            b.user_id as user2_id,
+            a.id AS activity1_id,
+            b.id AS activity2_id,
+            GREATEST(a.start_date_time, b.start_date_time) AS overlap_start,
+            LEAST(a.end_date_time, b.end_date_time) AS overlap_end
+        FROM 
+            Activity a
+        JOIN 
+            Activity b ON a.user_id != b.user_id
+                      AND (
+                          (a.start_date_time <= b.end_date_time AND a.end_date_time >= b.start_date_time)
+                       OR (b.start_date_time <= a.end_date_time AND b.end_date_time >= a.start_date_time)
+                      )
+        )
+        SELECT 
+            o.user1_id,
+            o.activity1_id,
+            t1.id AS trackpoint1_id,
+            t1.lat AS trackpoint1_lat,
+            t1.lon AS trackpoint1_lon,
+            t1.date_time AS trackpoint1_date_time,
+            o.user2_id,
+            o.activity2_id,
+            t2.id AS trackpoint2_id,
+            t2.lat AS trackpoint2_lat,
+            t2.lon AS trackpoint2_lon,
+            t2.date_time AS trackpoint2_date_time
+        FROM 
+            OverlappingActivities o
+        JOIN 
+            TrackPoint t1 ON o.activity1_id = t1.activity_id
+        JOIN 
+            TrackPoint t2 ON o.activity2_id = t2.activity_id
+        WHERE 
+            t1.date_time BETWEEN o.overlap_start AND o.overlap_end
+        AND 
+            t2.date_time BETWEEN o.overlap_start AND o.overlap_end
+        AND
+            ABS(TIMESTAMPDIFF(SECOND, t1.date_time, t2.date_time)) <= 30
+        AND
+            ST_DISTANCE_SPHERE(POINT(t1.lon,t1.lat), POINT(t2.lon, t2.lat)) <= 50
+            LIMIT 100000
+            
+    """)
+    df = pd.read_sql_query(query_entire, program.db_connection)
+
+    #df = pd.read_csv("pls_work.csv.csv")
+
+    processed = 0
+    total = len(df)
+    close_users = set()
+    for _, row in df.iterrows():
+        user_pair = tuple(sorted([row["user1_id"], row["user2_id"]]))
+        if user_pair in close_users:
+            processed += 1
+            continue
+        coords1 = (row["trackpoint1_lat"], row["trackpoint1_lon"])
+        coords2 = (row["trackpoint2_lat"], row["trackpoint2_lon"])
+        distance = haversine(coords1, coords2, unit="m")
+        if distance <= 50:
+            close_users.add(user_pair)
+            processed += 1
+        print(f"Processed {processed}/{total} rows", end="\r")
+    print(close_users)
+
+
+
+
+if __name__ == '__main__':
+    program = None
+    try:
+        program = task2()
+        #query2(program)
+        #query5(program)
+        query8(program)
+        #query11(program)
+        # Load data
 
     except Exception as e:
-
         print("ERROR: Failed to use database:", e)
 
     finally:
         if program:
             program.connection.close_connection()
-
-
-if __name__ == '__main__':
-    main()
